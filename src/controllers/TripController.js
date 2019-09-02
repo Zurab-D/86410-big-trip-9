577e534620241c95b73b97d3d9742fbd7ce9b6ac
Zurab Diakonashvili <zurab.diakonashvili@gmail.com>
2019-09-02 20:25:15 +0300

    fix

--- src/controllers/TripController.js
+++ src/controllers/TripController.js
@@ -132,7 +132,7 @@ export class TripController {
 
     // re-render header
     unrender(this._trip.element);
-    this._trip.removeElement()
+    this._trip.removeElement();
     render(this.elemTripMain, this._trip.element, Position.afterBegin);
   }
 }
